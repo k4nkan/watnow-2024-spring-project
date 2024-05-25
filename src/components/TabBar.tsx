08c631d579fe2e609c90098ba231a9b45b8a4794
chell-uoxou <chelluoxou@gmail.com>
2024-05-25 15:38:44 +0900

    feat: クリックしたら移動できるように、現在いる場所がハイライトされるように

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -3,13 +3,39 @@ import { Gear, House, PencilSimple } from '@phosphor-icons/react';
 import TabItem from './TabItem';
 import { HStack } from '@chakra-ui/react';
 
-const TabBar = () => {
+interface Props {
+  height?: string;
+}
+
+const TabBar = (props: Props) => {
+  const height = props.height ?? '80px';
   return (
     <>
-      <HStack justifyContent={'space-evenly'}>
-        <TabItem title="ホーム" icon={<House size={32} />} />
-        <TabItem title="連絡" icon={<PencilSimple size={32} />} />
-        <TabItem title="設定" icon={<Gear size={32} />} />
+      <HStack
+        justifyContent={'space-evenly'}
+        w={'100%'}
+        h={height}
+        borderTopWidth={1}
+        borderColor={'gray.50'}
+      >
+        <TabItem
+          title="ホーム"
+          activeIcon={<House size={32} weight={'fill'} />}
+          icon={<House size={32} />}
+          href="/home"
+        />
+        <TabItem
+          title="連絡"
+          activeIcon={<PencilSimple size={32} weight={'fill'} />}
+          icon={<PencilSimple size={32} />}
+          href="/update"
+        />
+        <TabItem
+          activeIcon={<Gear size={32} weight={'fill'} />}
+          title="設定"
+          icon={<Gear size={32} />}
+          href="/settings"
+        />
       </HStack>
     </>
   );
--- src/components/TabItem.tsx
+++ src/components/TabItem.tsx
@@ -1,26 +1,37 @@
-import {
-  Box,
-  VStack
-} from '@chakra-ui/react';
+import { Box, VStack } from '@chakra-ui/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { ReactElement } from 'react';
 
 interface titleProps {
   title: string;
   icon: ReactElement;
+  activeIcon: ReactElement;
+  href: string;
 }
 
 const TabItem = (props: titleProps) => {
+  const pathName = usePathname();
+
+  const isActive = (href: string) => {
+    return pathName.startsWith(href);
+  };
+
   return (
-    <>
-      <VStack w={'60px'} spacing={1}>
+    <Link href={props.href}>
+      <VStack
+        w={'60px'}
+        spacing={1}
+        className={isActive(props.href) ? 'text-orange-500' : undefined}
+      >
         <Box h={8} w={8}>
-          {props.icon}
+          {isActive(props.href) ? props.activeIcon : props.icon}
         </Box>
         <Box>
           <p className="h-5 text-xs font-bold">{props.title}</p>
         </Box>
       </VStack>
-    </>
+    </Link>
   );
 };
 
