961e1a4e8b172146fea53735d721175e1729eab1
chell-uoxou <chelluoxou@gmail.com>
2024-05-25 14:27:14 +0900

    fix: タブバーのスタイルを修正

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -11,7 +11,13 @@ const TabBar = (props: Props) => {
   const height = props.height ?? '80px';
   return (
     <>
-      <HStack justifyContent={'space-evenly'} w={'100%'} h={height}>
+      <HStack
+        justifyContent={'space-evenly'}
+        w={'100%'}
+        h={height}
+        borderTopWidth={1}
+        borderColor={'gray.50'}
+      >
         <TabItem title="ホーム" icon={<House size={32} />} />
         <TabItem title="連絡" icon={<PencilSimple size={32} />} />
         <TabItem title="設定" icon={<Gear size={32} />} />
