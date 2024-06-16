238e8a9a369929e948c5ab4e9ef5baecbca87849
k4nkan <cylbxt126@gmail.com>
2024-06-17 03:13:59 +0900

    ダミーのコンポーネントを配置できました！

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+import { Stack, Text, VStack } from '@chakra-ui/react';
+import PageTitle from '@/components/PageTitle';
+import AlertBox from '@/components/AlertBox';
+import TitleWithIcon from '@/components/TitleWithIcon';
+import { Lightbulb, Onigiri } from '@phosphor-icons/react';
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <>
+      <VStack w={'100%'} px={6} py={2.5}>
+        <PageTitle titleText="ホーム" />
+        <TitleWithIcon
+          text="今日のご飯予報"
+          ion={<Onigiri size={24} weight="bold" />}
+        />
+        <TitleWithIcon
+          text="ヒント"
+          ion={<Lightbulb size={24} weight="bold" />}
+        />
+        <AlertBox />
+      </VStack>
+    </>
+  );
 };
 
 export default Page;
