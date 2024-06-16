605b2ab1b2a03999fcb88912f6693cb0d190d4e3
chell-uoxou <chelluoxou@gmail.com>
2024-06-17 05:15:11 +0900

    style: インデントをスペース2行でフォーマット

--- src/components/TitleWithIcon.tsx
+++ src/components/TitleWithIcon.tsx
@@ -2,21 +2,19 @@ import { Box, Center, HStack, Heading } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 
 interface Props {
-    text: string;
-    ion: ReactElement;
+  text: string;
+  ion: ReactElement;
 }
 
-const TitleWithIcon = (Props : Props) => {
-    return (
-        <HStack mt={3} w={'100%'} justifyContent={'start'}>
-            <Center h={8} w={8}>
-                {Props.ion}
-            </Center>
-            <Heading size={'md'}>
-                {Props.text}
-            </Heading>
-        </HStack>
-    )
-}
+const TitleWithIcon = (Props: Props) => {
+  return (
+    <HStack mt={3} w={'100%'} justifyContent={'start'}>
+      <Center h={8} w={8}>
+        {Props.ion}
+      </Center>
+      <Heading size={'md'}>{Props.text}</Heading>
+    </HStack>
+  );
+};
 
 export default TitleWithIcon;
