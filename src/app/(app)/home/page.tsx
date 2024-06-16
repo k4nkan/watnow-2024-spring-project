4abcd6cbde3bf22a93d2994526ed1d43bf37d34a
k4nkan <cylbxt126@gmail.com>
2024-06-17 04:08:31 +0900

    ご飯予報のコンポーネントをホームのページに追加

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { Stack, Text, VStack } from '@chakra-ui/react';
 import PageTitle from '@/components/PageTitle';
-import AlertBox from '@/components/AlertBox';
 import TitleWithIcon from '@/components/TitleWithIcon';
 import { Lightbulb, Onigiri } from '@phosphor-icons/react';
+import AlertContainer from '@/components/AlertContainer';
+import ForecastStatusContainer from '@/components/ForecastStatusContainer';
 
 const Page = () => {
   return (
@@ -14,11 +15,12 @@ const Page = () => {
           text="今日のご飯予報"
           ion={<Onigiri size={24} weight="bold" />}
         />
+        <ForecastStatusContainer />
         <TitleWithIcon
           text="ヒント"
           ion={<Lightbulb size={24} weight="bold" />}
         />
-        <AlertBox />
+        <AlertContainer />
       </VStack>
     </>
   );
