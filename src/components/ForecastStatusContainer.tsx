8e16e8234b8e3e1ae95371018232c830e6d3bc5e
k4nkan <cylbxt126@gmail.com>
2024-06-17 04:06:12 +0900

    コンポーネント名の変更

--- src/components/ForecastStatusContainer.tsx
+++ src/components/ForecastStatusContainer.tsx
@@ -1,7 +1,7 @@
 import { Pending } from './forecast-status-block/PendingStatus';
 
-const ForecastStatusBlock = () => {
+const ForecastStatusContainer = () => {
   return <Pending current={4} max={10} />;
 };
 
-export default ForecastStatusBlock;
+export default ForecastStatusContainer;
