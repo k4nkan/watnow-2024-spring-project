489b5e6dad30ed66baa4a492118e691589af4877
chell-uoxou <chelluoxou@gmail.com>
2024-06-24 07:04:35 +0900

    feat: ForecastStatusLayoutをcolorSchemeに対応

--- src/components/forecast-status-block/PendingStatus.tsx
+++ src/components/forecast-status-block/PendingStatus.tsx
@@ -13,6 +13,7 @@ export const Pending = (props: PendingProps) => {
     <ForecastStatusLayout
       badgeText="未確定"
       description={'連絡がまだの人がいます！'}
+      colorScheme="gray"
     >
       <Progress
         colorScheme="progress"
@@ -21,8 +22,12 @@ export const Pending = (props: PendingProps) => {
         value={progressValue}
       />
       <HStack width="100%" justifyContent="space-between">
-        <Text><span className='font-bold'>{props.current}</span>人</Text>
-        <Text>あと <span className='font-bold'>{props.max - props.current}</span>人</Text>
+        <Text>
+          <span className="font-bold">{props.current}</span>人
+        </Text>
+        <Text>
+          あと <span className="font-bold">{props.max - props.current}</span>人
+        </Text>
       </HStack>
     </ForecastStatusLayout>
   );
