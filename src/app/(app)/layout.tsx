b5561eb4e3c5cc2a776a1f1190d0bf7dd76e29b0
k4nkan <cylbxt126@gmail.com>
2024-06-26 00:51:26 +0900

    スクロールを状態で管理、てっぺんにいるかどうか

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,12 +5,25 @@ import { NavigationBar } from '@/features/navigation-bar/NavigationBar';
 import { useCurrentUser } from '@/hooks/use-current-user';
 import { Box, Center, Spinner, VStack } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, UIEventHandler, useEffect, useState } from 'react';
 
 export default function AppLayout({ children }: PropsWithChildren) {
   const { currentUser, loading: loadingCurrentUser } = useCurrentUser();
   const [viewAvailable, setViewAvailable] = useState(false);
   const router = useRouter();
+  const [isTop, setIsTop] = useState(true);
+
+  console.log('Rendered!', isTop);
+
+  const scrollEvent: UIEventHandler = (uiEvent) => {
+    if (uiEvent.target instanceof HTMLElement) {
+      if (uiEvent.target.scrollTop !== 0) {
+        setIsTop(false);
+      } else {
+        setIsTop(true);
+      }
+    }
+  };
 
   useEffect(() => {
     if (!loadingCurrentUser) {
@@ -31,7 +44,7 @@ export default function AppLayout({ children }: PropsWithChildren) {
       ) : (
         <VStack justify={'space-between'} h={'100%'} w={'100%'}>
           <NavigationBar />
-          <Box overflow={'auto'} h={'100%'} w={'100%'}>
+          <Box overflow={'auto'} h={'100%'} w={'100%'} onScroll={scrollEvent}>
             {children}
           </Box>
           <TabBar />
