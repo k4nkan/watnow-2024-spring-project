b3b74500db690f61e54c338c64821d7fa061a97c
k4nkan <cylbxt126@gmail.com>
2024-06-26 05:04:06 +0900

    ページ上部かつスクロール可能な際にボタンを表示

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,27 +1,50 @@
 'use client';
 
+import PageDownButton from '@/components/PageDownButton';
 import TabBar from '@/components/TabBar';
 import { NavigationBar } from '@/features/navigation-bar/NavigationBar';
 import { useCurrentUser } from '@/hooks/use-current-user';
 import { Box, Center, Spinner, VStack } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
-import { PropsWithChildren, UIEventHandler, useEffect, useState } from 'react';
+import {
+  PropsWithChildren,
+  UIEventHandler,
+  useEffect,
+  useRef,
+  useState
+} from 'react';
 
 export default function AppLayout({ children }: PropsWithChildren) {
   const { currentUser, loading: loadingCurrentUser } = useCurrentUser();
   const [viewAvailable, setViewAvailable] = useState(false);
   const router = useRouter();
   const [isTop, setIsTop] = useState(true);
+  const [isScrollable, setIsScrollable] = useState(false);
+  const resizeObserver = useRef<ResizeObserver | null>(null);
 
   console.log('Rendered!', isTop);
 
-  const scrollEvent: UIEventHandler = (uiEvent) => {
+  const handleScroll: UIEventHandler = (uiEvent) => {
     if (uiEvent.target instanceof HTMLElement) {
       if (uiEvent.target.scrollTop !== 0) {
         setIsTop(false);
       } else {
         setIsTop(true);
       }
+    }
+  };
+
+  const handleAppearScrollableAppContainer = (
+    element: HTMLDivElement | null
+  ) => {
+    if (element) {
+      resizeObserver.current = new ResizeObserver(() => {
+        console.log('resized!');
+        setIsScrollable(element.scrollHeight > element.clientHeight);
+      });
+      resizeObserver.current.observe(element);
+    } else {
+      resizeObserver.current?.disconnect();
     }
   };
 
@@ -44,9 +67,16 @@ export default function AppLayout({ children }: PropsWithChildren) {
       ) : (
         <VStack justify={'space-between'} h={'100%'} w={'100%'}>
           <NavigationBar />
-          <Box overflow={'auto'} h={'100%'} w={'100%'} onScroll={scrollEvent}>
+          <Box
+            overflow={'auto'}
+            h={'100%'}
+            w={'100%'}
+            onScroll={(e) => handleScroll(e)}
+            ref={handleAppearScrollableAppContainer}
+          >
             {children}
           </Box>
+          {isScrollable && isTop ? <PageDownButton /> : <></>}
           <TabBar />
         </VStack>
       )}
