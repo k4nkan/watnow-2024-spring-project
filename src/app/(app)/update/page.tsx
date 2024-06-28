92da1a7e5acd97a7d14d983b5a7b6db3535bded4
chell-uoxou <chelluoxou@gmail.com>
2024-06-29 05:43:00 +0900

    feat: 更新ボタンを追加

--- src/app/(app)/update/page.tsx
+++ src/app/(app)/update/page.tsx
@@ -6,7 +6,7 @@ import { Text, VStack } from '@chakra-ui/react';
 
 const Page = () => {
   return (
-    <VStack w={'100%'} px={6} py={2.5}>
+    <VStack w={'100%'} px={6} py={2.5} gap={4}>
       <PageTitle titleText="連絡" />
       <Text>
         今日の晩御飯はどれくらい食べる予定ですか？量や献立の希望を編集しましょう！
